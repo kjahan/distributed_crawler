abee6521740b5e7abfdf0103652d199898c6fd94
Kazem Jahanbakhsh <k.jahanbakhsh@gmail.com>
2019-06-30 15:11:08 -0700

    fixing scraping issue by adding the UA

--- helper.py
+++ helper.py
@@ -6,6 +6,8 @@ def setup(queue_name):
     channel.queue_declare(queue=queue_name, durable=True)
     return connection, channel
 
+
 def scrape(url):
-    r = requests.get(url)
+    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+    r = requests.get(url, headers=headers)
     return r.text
